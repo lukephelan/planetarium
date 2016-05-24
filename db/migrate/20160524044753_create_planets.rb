a1f6177b1ad27e0c27f1fecff46df53abd03ebbf
lukephelan <luke.phelan@hotmail.com>
2016-05-24 15:04:50 +1000

    Migrated, created planet table

--- db/migrate/20160524044753_create_planets.rb
+++ db/migrate/20160524044753_create_planets.rb
@@ -1,7 +1,16 @@
 class CreatePlanets < ActiveRecord::Migration
   def change
     create_table :planets do |t|
-
+      t.string :name
+      t.integer :diameter
+      t.boolean :solid
+      t.integer :order
+      t.boolean :rings, default: false
+      t.boolean :explored, default: false
+      t.integer :moons
+      t.string :image
+      t.string :atmosphere
+      t.integer :distance_from_sun
       t.timestamps null: false
     end
   end
