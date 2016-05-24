fab15f1d3d356cf6a2d052c820407025d1f9c313
lukephelan <luke.phelan@hotmail.com>
2016-05-24 21:11:28 +1000

    Added ability to delete entries

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,13 +1,27 @@
 class PlanetsController < ApplicationController
   def index
+    @planets = Planet.all
+  end
+
+  def show
+    @planet = Planet.find(params[:id])
+    # respond_to do |format|
+    #   format.html { render 'planets/show' }
+    #   format.json { render json: @planet.to_json }
+    # end
   end
 
   def new
     @planet = Planet.new
   end
 
+  def edit
+    @planet = Planet.find(params[:id])
+  end
+
   def create
     @planet = Planet.create planet_params
+
     if @planet.valid?
       redirect_to planets_path
     else
@@ -21,15 +35,21 @@ class PlanetsController < ApplicationController
     :explored, :image, :distance_from_sun, :atmosphere, :solid)
   end
 
-  def show
+  def update
     @planet = Planet.find(params[:id])
-    respond_to do |format|
-      format.html { render 'planets/show' }
-      format.json { render json: @planet.to_json }
+    if @planet.update(planet_params)
+      redirect_to @planet
+    else
+      redirect_to new_planet_path, :flash => { :errors =>
+        @planet.errors.full_messages.join('. ') }
     end
   end
 
-  def edit
+  def destroy
     @planet = Planet.find(params[:id])
+    @planet.destroy
+
+    redirect_to planets_path
   end
+  
 end
